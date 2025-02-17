04a1d6775c725a71326f3635d18c435a3cb6bf00
Rendi Ichtiar Prasetyo <rendiichtiarprasetyo@gmail.com>
2025-02-18 01:18:17 +0700

    fix db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,7 +6,10 @@ export async function connectDB() {
     port: Number(process.env.DB_PORT),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: {
+      rejectUnauthorized: true
+    }
   });
   
   return connection;
